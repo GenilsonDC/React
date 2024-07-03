96c62c5a16904633e16f0b1077265d7c943384d1
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-07-03 07:52:32 -0300

    Weather app  🌤️

--- clima-app/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/index.tsx
-import React from 'react';
-import LoadingScreen from '../components/loadingScreen';
-
-const Home: React.FC = () => {
-  return <LoadingScreen />;
-};
-
-export default Home;
--- clima-app/src/pages/location.tsx
+++
@@ -1,6 +0,0 @@
-import LocationScreen from "@/components/locationScreen";
-const Location: React.FC = () => {
-    return<LocationScreen/>
-}
-
-export default Location;
